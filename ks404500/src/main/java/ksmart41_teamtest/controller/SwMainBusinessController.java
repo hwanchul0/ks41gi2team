5420de65a2d1ae2acaa97a9f6777983343a2bfb4
82105 <82105@DESKTOP-A855I5G>
2021-12-29 09:57:45 +0900

    <유성> 사업장 조회 추가

--- ks404500/src/main/java/ksmart41_teamtest/controller/SwMainBusinessController.java
+++ ks404500/src/main/java/ksmart41_teamtest/controller/SwMainBusinessController.java
@@ -1,21 +1,34 @@
 package ksmart41_teamtest.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ksmart41_teamtest.dto.Business;
+import ksmart41_teamtest.service.BusinessService;
 
 @Controller
 @RequestMapping("/sw/business")
 public class SwMainBusinessController {
 	
+	@Autowired
+	private BusinessService businessService;
+	
+	
 	@GetMapping("/addBusiness")
 	public String addBusiness() {
 		return "sw/business/addBusiness";
 	}
-	
+	/* 유성 사업장 조회 */
 	@GetMapping("/selectBusiness")
-	public String selectBusiness() {
+	public String selectBusiness(Model model) {
+		List<Business> business = businessService.getBusiness();
+		model.addAttribute("title", "사업장 조회");
+		model.addAttribute("business", business);
+		System.out.println(business + "<-- controller");
 		return "sw/business/selectBusiness";
 	}
 	
