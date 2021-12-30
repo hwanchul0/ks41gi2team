a67a4d9730f74c17e47b1f5968ed97875c1bdf93
BAEK <BAEK@DESKTOP-S97Q0NL>
2021-12-30 16:41:17 +0900

    [재천]

--- ks404500/src/main/java/ksmart41_teamtest/controller/ShopClientController.java
+++ ks404500/src/main/java/ksmart41_teamtest/controller/ShopClientController.java
@@ -1,8 +1,5 @@
 package ksmart41_teamtest.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,43 +7,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ksmart41_teamtest.dto.Client;
-import ksmart41_teamtest.dto.Member;
 import ksmart41_teamtest.service.ClientService;
 
 @Controller
-@RequestMapping("/sw/client")
-public class SwClientController {
+@RequestMapping("/shop/client")
+public class ShopClientController {
 	
 	//의존성 주입
-	@Autowired
 	private ClientService clientService;
-
-	//[재천]외부 회원 등록 GET
+	
+	//[재천]회원 등록
 	@GetMapping("/addClient")
 	public String addClient(Model model) {
-		model.addAttribute("title", "회원 등록");
-		return "sw/client/addClient";
+		model.addAttribute("title", "회원등록");
+		return "shop/client/addClient";
 	}
 	
-	//[재천]외부 회원 전체 조회
+	//[재천]회원 조회
 	@GetMapping("/selectClient")
 	public String selectClient(Model model) {
-		List<Client> clientList = clientService.selectClientList();
-		
-		model.addAttribute("title", "회원 조회");
-		model.addAttribute("clientList", clientList);
-		return "sw/client/selectClient";
+		model.addAttribute("title", "회원조회");
+		return "shop/client/selectClient";
 	}
 	
-	//[재천]외부 회원 등록 POST
+	//post방식 addClient
 	@PostMapping("/addClient")
 	public String addClient(Client client) {
-		System.out.println(client + " : SwClientControoler에서 받아온 client값");
+		System.out.println("shopclientController에서 받은 값" + client);
 		
+		//insert처리
 		String clientId = client.getClientId();
 		if(clientId != null && !"".equals(clientId)) {
 			clientService.addClient(client);
 		}
-		return "redirect:/sw/client/selectClient";
+		return "redirect:/shop/client/selectClient";
 	}
 }
