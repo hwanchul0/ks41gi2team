752f86702251ee50c47b81fd3adbe39f6c2579e5
BAEK <BAEK@DESKTOP-S97Q0NL>
2022-01-19 09:28:17 +0900

    220119 백재천

--- ks404500/src/main/java/ksmart41_teamtest/controller/ShopServiceRequestController.java
+++ ks404500/src/main/java/ksmart41_teamtest/controller/ShopServiceRequestController.java
@@ -24,82 +24,82 @@ import ksmart41_teamtest.service.ServiceManagementService;
 @Controller
 @RequestMapping("/shop/service/serviceRequest")
 public class ShopServiceRequestController {
-	
-	private static final Logger log = LoggerFactory.getLogger(ShopServiceRequestController.class);
-	// 계약요청 관련 의존성 주입
-	private ServiceManagementService serviceManagementService;
-	
-	// 계약요청 관련 의존성 주입
-	private ServiceRequestService serviceRequestService;
-	
-	public ShopServiceRequestController(ServiceRequestService serviceRequestService , ServiceManagementService serviceManagementService) {
-		this.serviceRequestService = serviceRequestService;
-		this.serviceManagementService = serviceManagementService;
-	}
-	
-	// client /  계약요청 등록
-	@GetMapping("/addServiceRequest")
-	public String addServiceRequest(Model model) {
-		List<ServiceManagement> serviceManagement = serviceManagementService.getServiceManagement();
-		model.addAttribute("serviceManagement",serviceManagement);
-		model.addAttribute("title","계약요청 등록");
-		return "shop/service/serviceRequest/addServiceRequest";
-	}
-	@PostMapping("/addServiceRequest")
-	public String addServiceRequest(ServiceRequest serviceRequest) {
-		log.info("ShopServiceRequestController 계약요청 등록 화면에서 입력받은 값 :{}" + serviceRequest);
-		serviceRequestService.addServiceRequest(serviceRequest);
+   
+   private static final Logger log = LoggerFactory.getLogger(ShopServiceRequestController.class);
+   // 계약요청 관련 의존성 주입
+   private ServiceManagementService serviceManagementService;
+   
+   // 계약요청 관련 의존성 주입
+   private ServiceRequestService serviceRequestService;
+   
+   public ShopServiceRequestController(ServiceRequestService serviceRequestService , ServiceManagementService serviceManagementService) {
+      this.serviceRequestService = serviceRequestService;
+      this.serviceManagementService = serviceManagementService;
+   }
+   
+   // client /  계약요청 등록
+   @GetMapping("/addServiceRequest")
+   public String addServiceRequest(Model model) {
+      List<ServiceManagement> serviceManagement = serviceManagementService.getServiceManagement();
+      model.addAttribute("serviceManagement",serviceManagement);
+      model.addAttribute("title","계약요청 등록");
+      return "shop/service/serviceRequest/addServiceRequest";
+   }
+   @PostMapping("/addServiceRequest")
+   public String addServiceRequest(ServiceRequest serviceRequest) {
+      log.info("ShopServiceRequestController 계약요청 등록 화면에서 입력받은 값 :{}" + serviceRequest);
+      serviceRequestService.addServiceRequest(serviceRequest);
 
-		return "redirect:/shop/service/serviceRequest/selectServiceRequest";
-	}
-	// client / 계약요청 등록 /  내 사업장별 대표코드 조회하기 modal
-	@PostMapping("/findMyMainBizCode")
-	@ResponseBody
-	public List<Map<String, Object>> findMyMainBizCode(String shopMemberId){
-		List<Map<String, Object>> myMainBizCode = serviceRequestService.findMyMainBizCode(shopMemberId);
-		log.info("PostMapping 계약요청 등록 - 내 사업장별 대표코드 조회 결과 :{}" , myMainBizCode);
-		return myMainBizCode;
-	}
-	
-	// client 본인 계약요청 조회
-	@GetMapping("/selectServiceRequest") 
-	  public String getRequestList(Model model, HttpSession session) { 
-		String SHOPID = (String) session.getAttribute("SHOPID");
-		//로그인한 쇼핑몰 Id 세션값
-		List<ServiceRequest> getRequestList = serviceRequestService.getRequestList();
-		model.addAttribute("title", "계약요청 개인조회"); 
-		model.addAttribute("getRequestList", getRequestList);
+      return "redirect:/shop/service/serviceRequest/selectServiceRequest";
+   }
+   // client / 계약요청 등록 /  내 사업장별 대표코드 조회하기 modal
+   @PostMapping("/findMyMainBizCode")
+   @ResponseBody
+   public List<Map<String, Object>> findMyMainBizCode(String shopMemberId){
+      List<Map<String, Object>> myMainBizCode = serviceRequestService.findMyMainBizCode(shopMemberId);
+      log.info("PostMapping 계약요청 등록 - 내 사업장별 대표코드 조회 결과 :{}" , myMainBizCode);
+      return myMainBizCode;
+   }
+   
+   // client 본인 계약요청 조회
+   @GetMapping("/selectServiceRequest") 
+     public String getRequestList(Model model, HttpSession session) { 
+      String SHOPID = (String) session.getAttribute("SHOPID");
+      //로그인한 쇼핑몰 Id 세션값
+      List<ServiceRequest> getRequestList = serviceRequestService.getRequestList();
+      model.addAttribute("title", "계약요청 개인조회"); 
+      model.addAttribute("getRequestList", getRequestList);
 
-		return "shop/service/serviceRequest/selectServiceRequest"; 
-	  }
-	// client 계약요청 수정 ( 개발사에서 승인 , 취소 전에 수정 가능)
-	@PostMapping("/modifyServiceRequest")
-	public String modifyServiceRequest(ServiceRequest serviceRequest) {
-		log.info("PostMapping 계약요청 수정 페이지 입력 받은 값 ======== {}:",serviceRequest);
-		
-		serviceRequestService.modifyServiceRequest(serviceRequest);
-		
-		return "redirect:/shop/service/serviceRequest/selectServiceRequest";
-	}
-	@GetMapping("/modifyServiceRequest")
-	public String modifyServiceRequest(@RequestParam(value="contractManageCode", required = false) String contractManageCode
-										,Model model) {
-		log.info("GetMapping 계약요청 수정페이지 contractManageCode ====== {}: " , contractManageCode);
-		model.addAttribute("title","계약요청 수정");
-		model.addAttribute("serviceInfo", serviceManagementService.getServiceManagement());
-		
-		if(contractManageCode != null && !"".equals(contractManageCode)) {
-			ServiceRequest getContractInfo = serviceRequestService.getContractInfo(contractManageCode);
-			model.addAttribute("getContractInfo",getContractInfo);
-		}
-		return "shop/service/serviceRequest/modifyServiceRequest";
-	}
-	// Client 계약요청 삭제 ( 관리자 승인 전 가능 )
-	@RequestMapping("/deleteServiceRequest")
-	public @ResponseBody int deleteServiceRequest(ServiceRequest ServiceRequest) {
-		log.info("계약요청 삭제 페이지 입력 받은 값 ======== {}:");
-		return serviceRequestService.deleteServiceRequest(ServiceRequest);
-	}
-	
-	
+      return "shop/service/serviceRequest/selectServiceRequest"; 
+     }
+   // client 계약요청 수정 ( 개발사에서 승인 , 취소 전에 수정 가능)
+   @PostMapping("/modifyServiceRequest")
+   public String modifyServiceRequest(ServiceRequest serviceRequest) {
+      log.info("PostMapping 계약요청 수정 페이지 입력 받은 값 ======== {}:",serviceRequest);
+      
+      serviceRequestService.modifyServiceRequest(serviceRequest);
+      
+      return "redirect:/shop/service/serviceRequest/selectServiceRequest";
+   }
+   @GetMapping("/modifyServiceRequest")
+   public String modifyServiceRequest(@RequestParam(value="contractManageCode", required = false) String contractManageCode
+                              ,Model model) {
+      log.info("GetMapping 계약요청 수정페이지 contractManageCode ====== {}: " , contractManageCode);
+      model.addAttribute("title","계약요청 수정");
+      model.addAttribute("serviceInfo", serviceManagementService.getServiceManagement());
+      
+      if(contractManageCode != null && !"".equals(contractManageCode)) {
+         ServiceRequest getContractInfo = serviceRequestService.getContractInfo(contractManageCode);
+         model.addAttribute("getContractInfo",getContractInfo);
+      }
+      return "shop/service/serviceRequest/modifyServiceRequest";
+   }
+   // Client 계약요청 삭제 ( 관리자 승인 전 가능 )
+   @RequestMapping("/deleteServiceRequest")
+   public @ResponseBody int deleteServiceRequest(ServiceRequest ServiceRequest) {
+      log.info("계약요청 삭제 페이지 입력 받은 값 ======== {}:");
+      return serviceRequestService.deleteServiceRequest(ServiceRequest);
+   }
+   
+   
 }
